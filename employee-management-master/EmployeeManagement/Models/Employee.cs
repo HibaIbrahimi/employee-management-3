908767d7a8959791332f04247f72440a78743862
HIBA <ibrahimih.hiba@gmail.com>
2026-01-30 01:17:03 +0400

    Initial commit

---
+++ employee-management-master/EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Adress")]
+        
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Length(8, 20)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
+        public string Position { get; set; }
+    }
+}
